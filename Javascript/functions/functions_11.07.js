f713fd3d1dc660060d09eea4a32bc48079d0ed28
AlexandrMel <melnic.alexandr88@gmail.com>
2019-07-11 11:09:11 +0200

    LowerCase and reverse functions

--- Javascript/functions/functions_11.07.js
+++ Javascript/functions/functions_11.07.js
@@ -3,19 +3,31 @@ function checkIfPalindrom (phrase) {
 //We take the string
 //We make it lowercase of uppercase
 transformToLowercase(phrase);
+let revesedPhrase = reversePhrase(phrase);
 
 //We produce the reversed string
-let reverserdPhrase = reversePhrase(phrase);
+//let reverserdPhrase = reversePhrase(phrase);
 // We check if original and reverse strings are identical
-comparePhrase()
+//comparePhrase()
 
 
 }
 function transformToLowercase(str) {
-    return
+    let ls = str.toLowerCase();
+    console.log(ls)
+    return ls;
+}
+
+function reversePhrase(sent) {
+    let res ='';
+    let l = sent.length;
+    for(i=(l-1);i>=0; i--) {
+        res = res + sent[i];
+        console.log(res)
+    }
 }
 
 checkIfPalindrom('level')   //Return true
 checkIfPalindrom('mAdaM')   //Return true, but send a message that complainsabout the input
-checkIfPalindrom('babylon') //Return false
+checkIfPalindrom('baBYlon') //Return false
 //checkIfPalindrom('i level madam level i')  // Return true
