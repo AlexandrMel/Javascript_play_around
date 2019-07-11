4e66d9e2640d9d88662142ff8d9d4995cacf75b9
AlexandrMel <melnic.alexandr88@gmail.com>
2019-07-11 09:48:12 +0200

    introduced the tasks

---
+++ Javascript/functions/functions_11.07.js
@@ -0,0 +1,14 @@
+//We take the string
+//We make it lowercase of uppercase
+//We produce the reversed string
+// We check if original and reverse strings are identical
+
+function checkIfPalindrom (string) {
+
+
+}
+
+checkIfPalindrom('level')   //Return true
+checkIfPalindrom('mAdaM')   //Return true, but send a message that complainsabout the input
+checkIfPalindrom('babylon') //Return false
+//checkIfPalindrom('i level madam level i')  // Return true
