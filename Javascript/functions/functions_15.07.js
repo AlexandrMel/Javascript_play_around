c18e85db83e62c9c98e019345d096edbc32beeaa
AlexandrMel <melnic.alexandr88@gmail.com>
2019-07-15 13:08:19 +0200

    some more exercise

--- Javascript/functions/functions_15.07.js
+++ Javascript/functions/functions_15.07.js
@@ -1,10 +1,10 @@
 //Write a function that will take as input the lengths of two sides of a triangle and it will return the length of the third side (Pythagorean).
 
-const pythagorean =(side1, side2) => {
+const pythagorean = (side1, side2) => {
     let result = Math.sqrt(Math.pow(side1, 2) + Math.pow(side2, 2));
     return result;
 }
-console.log(pythagorean(6,8));
+console.log(pythagorean(6, 8));
 
 
 //Write a function that will capitalize each word we give it.
@@ -19,7 +19,7 @@ console.log(capitalizeWord('mad'));
 //Build a pyramid
 // function addSpace(i,spaceNum,res)
 // {
- 
+
 //     for(j=0; j<spaceNum-i; j++){
 //         res = " " + res;
 //     }
@@ -36,10 +36,10 @@ console.log(capitalizeWord('mad'));
 //     let res;
 //     for(i=0; i<num; i++){
 //         res = "*";
-      
+
 //         // we add spaces
 //        res =addSpace(i,num,res);
-      
+
 //        // we add stars
 //        res =addStar(res);
 //         console.log(res);
@@ -47,11 +47,42 @@ console.log(capitalizeWord('mad'));
 // }
 // pyramid(0);
 
+// Calculate age
 
-function yearBorn(x) {
-    let resu = 2019 - x;
-    return resu;
+function calcAge(yearBorn)
+{
+ //set of the statements
 
+ //Way : 1
+ // let res;
+ // res=2019-yearBorn;
+ // return res;
 
+ //Way : 2
+ //return 2019-yearBorn;
+ console.log(typeof(yearBorn));
+ yearBorn = typeof(yearBorn)=="number" ? yearBorn : 0;
+ if(yearBorn==0)
+ {
+    return "Sorry, we can not calculate your age with this parameter so kindly provide integer parameter!";
+ }
+ else
+ {
+   yearBorn=Number.isInteger(yearBorn) ? yearBorn : Math.round(yearBorn);
+   return (2019-yearBorn);
+ }
 }
-console.log(yearBorn('1988'));
+console.log(calcAge(1982));
+
+
+//Calculate number greater then 10 or less then 10
+function greaterTen(n) {
+    // if (n > 10) {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    return (n>10);
+}
+console.log(greaterTen(11));
+
