e721589e2640a1ffab57722e808bde4ac4a59313
AlexandrMel <melnic.alexandr88@gmail.com>
2019-07-17 20:57:22 +0200

    some changes

--- Javascript/functions/function_tutoring_17.07.js
+++ Javascript/functions/function_tutoring_17.07.js
@@ -26,6 +26,14 @@ console.log(newF(10, 5));
 // TASK 2
 // Create a function that takes in a word and determines whether or not it is plural. A plural word is one that ends in "s".
 
+function checkForPlurals(word) {
+
+    console.log(word[(word.length)-1].includes('s'))
+}
+checkForPlurals('alexandrs')
+
+
+
 // TASK 3
 // Create a function that determines whether or not it's possible to split a pie fairly given these three parameters:
 // Total number of slices.
