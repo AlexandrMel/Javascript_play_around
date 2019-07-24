e5398954e5a8dcf459b354f313f68fbee3132c2c
AlexandrMel <melnic.alexandr88@gmail.com>
2019-07-24 12:48:25 +0200

    practicing with arrays looping

--- Javascript/arrays/arrays_exercise.js
+++ Javascript/arrays/arrays_exercise.js
@@ -1,1 +1,25 @@
-alert("Hello World")
+
+// var todos = ["Buy New Turtle"];
+
+// var input = prompt("What would you like to do?");
+
+
+// while(input !== "quit") {
+//     if(input === "list") {
+//         console.log(todos);
+//     }
+//     else if(input === "new") {
+    
+//         var newTodo = prompt("Enter new todo");
+    
+//         todos.push(newTodo);
+//     }
+//     input = prompt("What would you like to do?");
+// }
+
+// console.log("OK,YOU QUIT THE APP");
+
+var colors = ["red","green","orange","blue"];
+for(var i = 0; i<colors.length; i++) {
+    alert(colors[i]);
+}
