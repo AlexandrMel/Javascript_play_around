2755b968289d14bd43dab2f0ce62cfc4a590c728
AlexandrMel <melnic.alexandr88@gmail.com>
2019-08-05 16:25:47 +0200

    More Calling functions inside a function exercise

--- Javascript/functions/functions_05.08.js
+++ Javascript/functions/functions_05.08.js
@@ -29,3 +29,37 @@ detailsCalculate(name, 1985, calculateAge);
 detailsCalculate(name, 1985, isFullAge);
 detailsCalculate(name, 1989, maxHeartRate);
 
+
+//Task : Write Javascript function that accepts three arguments, a string,letter and function. Passed Function as argument will count the number of occurrences of the specified letter within the string.
+//Sameple arguments, "Iamstudent at DCI", 't',countWord
+//Expected output: 3
+
+function calcLetter( string, letter, funct) {
+    console.log(` The string ${string} has the letter ${letter} inside ${funct(string, letter)}`)
+
+}
+
+function calcFunc(str, x) {
+    var string = str.toLowerCase();
+   var x = x.toLowerCase()
+    var result = string.split(x).length-1;
+    console.log(typeof(result));
+    return result;
+}
+function calcF (string, letter) {
+    string = string.toLowerCase();
+    letter = letter.toLowerCase();
+    let n = 0;
+    
+    for(let i=0; i<string.length; i++){
+        if(string.charAt(i) === letter)  n+=1;
+    }
+        
+        return n;
+    }
+calcLetter("I am superman i i i", "i", calcFunc);
+calcLetter("I am supermanN i i i n N", "N", calcF);
+
+
+
+
