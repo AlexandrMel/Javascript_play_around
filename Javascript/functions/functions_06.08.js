a488e14657bb10b128dd3a22498e083c2fecd158
AlexandrMel <melnic.alexandr88@gmail.com>
2019-08-06 11:04:02 +0200

    Exercise with functions for comparing parts of strinfs

---
+++ Javascript/functions/functions_06.08.js
@@ -0,0 +1,35 @@
+//Task : Write Javascript function that accepts two arguments, a string and function. Passed Function as argument will check the string and find the not repeated character from string and return that not repeated all characters.
+//Sample arguments, "axbyazcddbec", findNotRepeated
+//Expected output: e
+
+
+
+function checkRepeat(arg, funct) {
+    console.log(`In the string ${arg} the not repeated letters are ${funct(arg)}`);
+}
+// var arg = "hahaha";
+// string = arg.split("");
+// console.log(string);
+
+
+// string.forEach(function(str) {
+//     for(i=1;i<arg.length; i++) {
+//         console.log(string);
+//         console.log(string[i]);
+//         if(str === string[i]) {
+//             console.log(str);
+//         }
+//     }
+//  });
+function searchRepeat(arg) {
+    for (i = 0; i < arg.length; i++) {
+       for(j= arg.length-1; j>1, j--)
+        
+        
+    }
+}       
+        
+
+searchRepeat("mmmmrum");
+searchRepeat("xmmmlum");
+searchRepeat("mmmmlum");
