ce60bc0c2dd57041825bc790e949e328aa707719
AlexandrMel <melnic.alexandr88@gmail.com>
2019-08-06 11:55:22 +0200

    new solutions

--- Javascript/functions/functions_06.08.js
+++ Javascript/functions/functions_06.08.js
@@ -1,35 +1,27 @@
-//Task : Write Javascript function that accepts two arguments, a string and function. Passed Function as argument will check the string and find the not repeated character from string and return that not repeated all characters.
-//Sample arguments, "axbyazcddbec", findNotRepeated
-//Expected output: e
-
-
-
-function checkRepeat(arg, funct) {
-    console.log(`In the string ${arg} the not repeated letters are ${funct(arg)}`);
+function repeatCharaters(strParam, funParam)
+{
+console.log(`Not Repeated Character: ${funParam(strParam)}`)
 }
-// var arg = "hahaha";
-// string = arg.split("");
-// console.log(string);
-
-
-// string.forEach(function(str) {
-//     for(i=1;i<arg.length; i++) {
-//         console.log(string);
-//         console.log(string[i]);
-//         if(str === string[i]) {
-//             console.log(str);
-//         }
-//     }
-//  });
-function searchRepeat(arg) {
-    for (i = 0; i < arg.length; i++) {
-       for(j= arg.length-1; j>1, j--)
-        
-        
-    }
-}       
-        
-
-searchRepeat("mmmmrum");
-searchRepeat("xmmmlum");
-searchRepeat("mmmmlum");
+function findNotRepeated(strParam)
+{
+ //console.log(strParam.indexOf('e'));
+ //console.log(strParam.indexOf('e',11));
+ //for Bonus Task
+ var result='';
+ for(var i=0; i<strParam.length; i++)
+ {
+   var charValue=strParam.charAt(i);
+   var firstIndex=strParam.indexOf(charValue);
+   var secondIndex=strParam.lastIndexOf(charValue);
+   //if(strParam.indexOf(charValue)== i && strParam.indexOf(charValue,i+1)==-1)
+   if(strParam.indexOf(charValue)== strParam.lastIndexOf(charValue))
+   {
+     //Main Task
+     //return strParam[i];
+     //Bonus Task
+     result +=strParam[i];
+   }
+ }
+ return result;
+}
+repeatCharaters("axbyazcddbexcyzl",findNotRepeated);
