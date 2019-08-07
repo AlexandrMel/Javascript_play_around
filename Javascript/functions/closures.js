ad27f9b2bff696c0295a5f7089b11c6e8d30a9cf
AlexandrMel <melnic.alexandr88@gmail.com>
2019-08-07 13:15:32 +0200

    starting the recursivness topic

--- Javascript/functions/closures.js
+++ Javascript/functions/closures.js
@@ -20,14 +20,23 @@ function outer() {
 var b = 10;
 function inner(){
     var  a = 20;
-    console.log(a+b)
+    console.log(`a =${a} b=${b}`)
+    a++;
+    b++;
 }
 return inner;
 }
 
-var x = outer();
+var x = outer(); // x equals  to whatever outer function returns
 var y = outer();
 
-console.log(typeof(x));
+console.log(typeof(x)); // is a function
 
 x(); // way of calling the function outer stored in the x - variable;
+x(); // way of calling the function outer stored in the x - variable;
+x(); // way of calling the function outer stored in the x - variable;
+x(); // way of calling the function outer stored in the x - variable;
+
+console.log(x);
+console.dir(x);
+
