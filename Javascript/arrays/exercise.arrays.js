f7ed2d6de8f50a4cadea398fabe1665d245dba77
AlexandrMel <melnic.alexandr88@gmail.com>
2019-08-08 15:06:53 +0200

    did some serios exercise with arrays

--- Javascript/arrays/exercise.arrays.js
+++ Javascript/arrays/exercise.arrays.js
@@ -134,3 +134,32 @@ function changeEnough(change, amountDue) {
 console.log(changeEnough([2, 100, 0, 0], 14.11));
 console.log(changeEnough([10, 0, 0, 50], 13.85));
 console.log(changeEnough([2, 100, 0, 0], 14.11));
+
+
+function sumTwoSmallestNumbers(numbers) {  
+    //Code here
+    numbers = numbers.sort(function(a, b){return a-b});
+   sum = numbers[0]+numbers[1];
+  return sum;
+  }
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
+console.log(sumTwoSmallestNumbers([15, 28, 4, 2, 43]))// 6 ,
+console.log(sumTwoSmallestNumbers([3, 87, 45, 12, 7])) // 10 
+console.log(sumTwoSmallestNumbers([23, 71, 33, 82, 1]))//  24 
+console.log(sumTwoSmallestNumbers([52, 76, 14, 12, 4])) // 16 
+
+function sumTwoSmallestNumbers2(numbers) {  
+    var [ a, b ] = numbers.sort((a, b) => a - b)
+    return a + b
+}
+console.log(sumTwoSmallestNumbers([5, 8, 12, 19, 22]));
+  
+let arri = ["one", "two", "three"];
+var [a,b,c] = ["one", "two", "three"];
+console.log(a);
+console.log(b);
+console.log(c);
+var [d,e,f] = euroCities;
+console.log(d)
+console.log(e)
+console.log(f)
