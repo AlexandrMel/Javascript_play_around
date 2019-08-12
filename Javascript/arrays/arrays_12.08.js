6cd01d06cf693b710e16c17d29a649b265d28da5
AlexandrMel <melnic.alexandr88@gmail.com>
2019-08-12 16:40:55 +0200

    duplicate removal, ordering, missing number functions with arrays

--- Javascript/arrays/arrays_12.08.js
+++ Javascript/arrays/arrays_12.08.js
@@ -116,24 +116,27 @@ function findUniq(arr) {
 }
 findUniq([0.89, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
 console.log(`=======================================`)
+
+
 //Missing Number
 //Create a function that takes an array of numbers between 0 and 10 (excluding one number) and returns the missing number.
 //Examples:
 //missingNums([1, 2, 3, 4, 6, 7, 8, 9, 10]) ➞ 5
 //missingNums([7, 2, 3, 6, 5, 9, 1, 4, 8]) ➞ 10
 //missingNums([10, 5, 1, 2, 4, 6, 8, 3, 9]) ➞ 7
+//===========================================================================
 // CASE 1
 // let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-// let total = 55;
 // const missing = arr => {
-//     let sum = 0;
+//     let sum = 55;
 //     for(let i=0; i<arr.length; i++){
-//         sum+= arr[i];
+//         sum-= arr[i];
 //     }
-//     return total - sum;
+//     return sum;
 // }
 // let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 // const missing = arr => {
+//============================================================================
 //     //CASE 2
 //     arr = arr.sort(function(a, b){
 //         return a-b;
@@ -141,6 +144,7 @@ console.log(`=======================================`)
 //     for(let i=0; i<numbers.length; i++){  
 //             if(arr[i] != numbers[i]) return numbers[i];            
 //     }
+//============================================================================
     //CASE 3
 //     arr.push(0);
 //     let sortedArray = arr.sort();
@@ -159,11 +163,24 @@ console.log(`=======================================`)
 // console.log(missing([1, 2, 3, 4, 6, 7, 8, 9, 10]));
 // console.log(missing([7, 2, 3, 6, 5, 9, 1, 4, 8]));
 // console.log(missing([10, 5, 1, 2, 4, 6, 8, 3, 9]));
+//============================================================================
+// CASE 4
+
+// function findMissingItem(arr, numbers) {
+//     for (i = 0; i < numbers.length; i++) {
+//         if (arr.indexOf(numbers[i]) == -1) return numbers[i];
+//     }
+//     return `No missing Item!`;
+// }
+// console.log(findMissingItem([1, 2, 3, 4, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// console.log(findMissingItem([7, 2, 3, 6, 5, 9, 1, 4, 8], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+// console.log(findMissingItem([10, 5, 1, 2, 4, 6, 8, 3, 9], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+//=============================================================================
+//CASE 5
 
 function missingNumber(arr) {
-    arr = arr.sort(function (a, b) {
-        return a - b;
-    });
+    arr = arr.sort((a, b) => a - b);
+
     console.log(arr)
     for (i = 0; i <= arr.length; i++) {
         if (arr[i] !== (i + 1)) {
@@ -175,7 +192,24 @@ function missingNumber(arr) {
 console.log(missingNumber([1, 3, 5, 4, 9, 7, 8, 10, 6]))
 console.log(missingNumber([2, 3, 5, 4, 9, 7, 8, 10, 6]))
 console.log(missingNumber([1, 3, 5, 4, 2, 7, 8, 9, 10]))
-console.log(missingNumber([6, 3, 5, 4, 1, 7, 8, 9, 2, 11, 12, 10]))
+console.log(missingNumber([17, 3, 5, 4, 6, 7, 8, 9, 2, 11, 12, 13, 14, 15, 16, 20, 18, 19, 10]))
 // missingNumber([7, 2, 3, 6, 5, 9, 1, 4, 8])
 // missingNumber([1, 3, 5, 2, 9, 7, 4, 10, 6])
 // missingNumber([6, 3, 5, 2, 9, 7, 4, 10, 1])
+
+
+//Remove duplicate items from an array (ignore case sensitivity).
+var nums = [1, 2, 10, 2, 4, 5, 4, 7, 8, 7, 3, 6, 6, 6, 6, 6, 1, 9, 10, 10, 10, 10, "sprint", "Sprint"];
+//Output nums= [1, 2, 3, 4, 5, 6,7,8]
+
+
+function deleteDuplicates (arr) {
+    arr = arr.sort((a, b) => a - b);
+    for(i=0; i<arr.length; i++) {
+    typeof(arr[i]) =="string" ? arr[i]=arr[i].toLowerCase() : arr[i];
+        if(arr.indexOf(arr[i]) !== arr.lastIndexOf(arr[i]))
+        { arr.splice(i,1); i--};
+    }
+    console.log(arr);
+}
+deleteDuplicates(nums)
