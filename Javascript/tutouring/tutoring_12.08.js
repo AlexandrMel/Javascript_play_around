e2cc5e6a55fbc5e20aa0e0fed5006cc351876a9f
AlexandrMel <melnic.alexandr88@gmail.com>
2019-08-13 11:05:18 +0200

    New exercises with arrays

--- Javascript/tutouring/tutoring_12.08.js
+++ Javascript/tutouring/tutoring_12.08.js
@@ -97,4 +97,4 @@ function scramble(str1, str2) {
   else {console.log(`false`)}
 }
 console.log(scramble('rkqodlw','world'));
-console.log(scramble('cedewaraaossoqqyt','codewars'));
+console.log(scramble('cedewaraaossoqqyt','codewaars'));
