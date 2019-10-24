be6b5fa744e686b493bf099a95ae27d5e30abc19
AlexandrMel <melnic.alexandr88@gmail.com>
2019-10-24 17:55:10 +0200

    final versions of the game but still not cleaned code

--- Javascript/DOM/dice_game/main.js
+++ Javascript/DOM/dice_game/main.js
@@ -1,121 +1,200 @@
 let newGame = document.querySelector(".btn-new");
-let rollDIce = document.querySelector(".btn-roll")
-let hold = document.querySelector(".btn-hold")
+let rollDIce = document.querySelector(".btn-roll");
+let hold = document.querySelector(".btn-hold");
 let player_0_panel = document.querySelector(".player-0-panel");
 let player_1_panel = document.querySelector(".player-1-panel");
-let player1_g_score = document.querySelector(".pl-gsc-1")
-let player2_g_score = document.querySelector(".pl-gsc-2")
-let player1_score = document.querySelector(".pl-sc-1")
-let player2_score = document.querySelector(".pl-sc-2")
+let player1_g_score = document.querySelector(".pl-gsc-1");
+let player2_g_score = document.querySelector(".pl-gsc-2");
+let player1_score = document.querySelector(".pl-sc-1");
+let player2_score = document.querySelector(".pl-sc-2");
 let dices = document.querySelector(".dices");
 let view = document.querySelector("#view");
-let dice = document.querySelector("#dice")
-let player1Name = document.querySelector(".pl-n-1")
-let player2Name = document.querySelector(".pl-n-2")
-let body = document.querySelector("body");
-
-
+let view2 = document.querySelector("#view2");
+let dice = document.querySelector("#dice");
+let dice2 = document.querySelector("#dice2");
+let player1Name = document.querySelector(".pl-n-1");
+let player2Name = document.querySelector(".pl-n-2");
+let switchKey = document.querySelector("#switchContainer");
+let switchTab = document.querySelector("#switch");
+let winLimitInput = document.querySelector(".inputWin");
 let randomNumber = () => {
-    return Math.floor(Math.random() * 6) + 1;
+  return Math.floor(Math.random() * 6) + 1;
+};
+var buttonKey = true;
+document.querySelector(".gameRules").onclick = function() {
+  let gameRules = document.querySelector(".gameRules");
+  if (gameRules.innerHTML == "Check out the Game rules") {
+    document.querySelector(".flip-card-inner").classList.toggle("flip");
+    gameRules.innerHTML = "Go back to the Game";
+  } else {
+    document.querySelector(".flip-card-inner").classList.toggle("flip");
+    gameRules.innerHTML = "Check out the Game rules";
+  }
+};
+function forDiceRolling(event) {
+  event.preventDefault();
+  if (event.code == "Space") {
+    diceRolling();
+    event.stopPropagation();
+  }
 }
-rollDIce.addEventListener("keyup", function (event) {
-    // alert(event.code)
-    event.preventDefault();
+function forHoldPress(event) {
+  if (event.code == "KeyS") {
+    holdPress();
+    event.stopPropagation();
+  }
+}
+function forReload(event) {
+  if (event.code == "KeyN") {
+    Reload();
+  }
+}
+switcheru();
+switchKey.addEventListener("click", function(event) {
+  switcheru();
+});
 
-    if (event.code == "Space") {
-        diceRolling();
-    }
-});
-rollDIce.addEventListener("keyup", function (event) {
-    event.preventDefault();
-    if (event.code == "Enter") {
-        hold.onclick();
-    }
-});
-rollDIce.addEventListener("keyup", function (event) {
-    event.preventDefault();
-    if (event.code == "KeyN") {
-        newGame.onclick();
-    }
-});
-rollDIce.addEventListener("click", diceRolling);
-
+function switcheru() {
+  winLimitInput.value = "";
+  if (buttonKey == true) {
+    let body = document.querySelector("body");
+    switchTab.style.transform = "translateX(0px)";
+    document.querySelector("#mouse").classList.toggle("bold");
+    document.querySelector("#keyboard").classList.toggle("bold");
+    document.querySelector("body").onkeydown = null;
+    document.querySelector("body").onkeyup = null;
+    document.querySelector("html").onkeyup = null;
+    rollDIce.onclick = diceRolling;
+    hold.onclick = holdPress;
+    newGame.onclick = Reload;
+    buttonKey = false;
+    return buttonKey;
+  } else if (buttonKey == false) {
+    switchTab.style.transform = "translateX(37.5px)";
+    document.querySelector("#mouse").classList.toggle("bold");
+    document.querySelector("#keyboard").classList.toggle("bold");
+    rollDIce.onclick = null;
+    hold.onclick = null;
+    newGame.onclick = null;
+    document.querySelector("body").onkeydown = forReload;
+    document.querySelector("body").onkeyup = forHoldPress;
+    document.querySelector("html").onkeyup = forDiceRolling;
+    buttonKey = true;
+    return buttonKey;
+  }
+}
 function verifyScore() {
-    if (Number(player1_g_score.innerHTML) > Number(player2_g_score.innerHTML) && Number(player1_g_score.innerHTML) > 20) {
-        player1Name.innerHTML = "WINNER";
-    }
-    if (Number(player2_g_score.innerHTML) > Number(player1_g_score.innerHTML) && Number(player2_g_score.innerHTML) > 20) {
-        player2Name.innerHTML = "WINNER";
-    }
-    player1_score.innerHTML = 0;
-    player2_score.innerHTML = 0;
+  let winLimit = winLimitInput.value | 20;
+  if (
+    Number(player1_g_score.innerHTML) > Number(player2_g_score.innerHTML) &&
+    Number(player1_g_score.innerHTML) > winLimit
+  ) {
+    player1Name.innerHTML = "WINNER";
+    player1Name.style.fontWeight = "500";
+    player1_g_score.style.fontWeight = "500";
+    view.style.display = "none";
+    view2.style.display = "none";
+    player_0_panel.classList.add("winnerClass");
+    player1_g_score.classList.add("pulse");
+  }
+  if (
+    Number(player2_g_score.innerHTML) > Number(player1_g_score.innerHTML) &&
+    Number(player2_g_score.innerHTML) > winLimit
+  ) {
+    player2Name.innerHTML = "WINNER";
+    player2Name.style.fontWeight = "500";
+    player2_g_score.style.fontWeight = "500";
+    view.style.display = "none";
+    view2.style.display = "none";
+    player_1_panel.classList.add("winnerClass");
+    player2_g_score.classList.add("pulse");
+  }
+  resetScore();
 }
-function animate(num) {
-    if (num == 1) {
-        dice.style.transform = "rotateY(360deg)"
-    }
-    if (num == 2) {
-        dice.style.transform = "rotateY(-90deg)"
-    }
-    if (num == 3) {
-        dice.style.transform = "rotateY(180deg)"
-    }
-    if (num == 4) {
-        dice.style.transform = "rotateY(90deg)"
-    }
-    if (num == 5) {
-        dice.style.transform = "rotateX(-90deg)"
-    }
-    if (num == 6) {
-        dice.style.transform = "rotateX(90deg)"
-    }
+
+function animate(num, x) {
+  if (num == 1) {
+    x.style.transform = "rotateY(360deg)";
+    x.style["-webkit-transform"] = "rotateY(360deg)";
+  }
+  if (num == 2) {
+    x.style.transform = "rotateY(-90deg)";
+    x.style["-webkit-transform"] = "rotateY(-90deg)";
+  }
+  if (num == 3) {
+    x.style.transform = "rotateY(180deg)";
+    x.style["-webkit-transform"] = "rotateY(180deg)";
+  }
+  if (num == 4) {
+    x.style.transform = "rotateY(90deg)";
+    x.style["-webkit-transform"] = "rotateY(90deg)";
+  }
+  if (num == 5) {
+    x.style.transform = "rotateX(-90deg)";
+    x.style["-webkit-transform"] = "rotateX(-90deg)";
+  }
+  if (num == 6) {
+    x.style.transform = "rotateX(90deg)";
+    x.style["-webkit-transform"] = "rotateX(90deg)";
+  }
+}
+
+function resetScore() {
+  player_0_panel.classList.toggle("active");
+  player_1_panel.classList.toggle("active");
+  player1_score.innerHTML = 0;
+  player2_score.innerHTML = 0;
 }
 
 function diceRolling() {
-    if (player1Name.innerHTML == "WINNER" || player2Name.innerHTML == "WINNER") {
-
-    } else {
-        view.style.display = "block"
-        if (player_0_panel.classList.contains("active")) {
-            let nr = randomNumber();
-            dices.setAttribute("src", `dice-${nr}.png`);
-            animate(nr)
-            if (nr !== 1) {
-                player1_score.innerHTML = Number(player1_score.innerHTML) + nr;
-            } else {
-                player_0_panel.classList.toggle("active")
-                player_1_panel.classList.toggle("active")
-                player1_score.innerHTML = 0;
-            }
-        } else {
-            let nr = randomNumber();
-            dices.setAttribute("src", `dice-${nr}.png`);
-            animate(nr)
-            if (nr !== 1) {
-                player2_score.innerHTML = Number(player2_score.innerHTML) + nr;
-            } else {
-                player_0_panel.classList.toggle("active")
-                player_1_panel.classList.toggle("active")
-                player2_score.innerHTML = 0;
-            }
-        }
-    }
-}
-hold.onclick = function () {
+    rollDIce.blur();
+  if (player1Name.innerHTML == "WINNER" || player2Name.innerHTML == "WINNER") {
+  } else {
+    dice.style.display = "block";
+    dice2.style.display = "block";
+    let nr = randomNumber();
+    let nr2 = randomNumber();
     if (player_0_panel.classList.contains("active")) {
-        player1_g_score.innerHTML = Number(player1_g_score.innerHTML) + Number(player1_score.innerHTML);
-        player_0_panel.classList.toggle("active")
-        player_1_panel.classList.toggle("active")
-        player1_score.innerHTML = 0;
+      animate(nr, dice);
+      animate(nr2, dice2);
+      if (nr !== 1 && nr2 !== 1) {
+        setTimeout(function() {
+          player1_score.innerHTML = Number(player1_score.innerHTML) + nr + nr2;
+        }, 500);
+      } else {
+        resetScore();
+      }
     } else {
-        player2_g_score.innerHTML = Number(player2_g_score.innerHTML) + Number(player2_score.innerHTML);
-        player_0_panel.classList.toggle("active")
-        player_1_panel.classList.toggle("active")
-        player2_score.innerHTML = 0;
+      animate(nr, dice);
+      animate(nr2, dice2);
+      if (nr !== 1 && nr2 !== 1) {
+        setTimeout(function() {
+          player2_score.innerHTML = Number(player2_score.innerHTML) + nr + nr2;
+        }, 500);
+      } else {
+        resetScore();
+      }
     }
-    verifyScore();
-}
-newGame.onclick = function () {
-    location.reload(true)
+  }
 }
 
+function holdPress() {
+    hold.blur();
+  if (player_0_panel.classList.contains("active")) {
+    player1_g_score.innerHTML =
+      Number(player1_g_score.innerHTML) + Number(player1_score.innerHTML);
+    resetScore();
+  } else {
+    player2_g_score.innerHTML =
+      Number(player2_g_score.innerHTML) + Number(player2_score.innerHTML);
+    resetScore();
+  }
+  verifyScore();
+  resetScore();
+}
+
+function Reload() {
+    newGame.blur()
+  winLimitInput.value = "";
+  location.reload();
+}
